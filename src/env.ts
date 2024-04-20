c014d4789471344a82e2b95f24632bfc5a2f1bcf
Lubnnia <lubnnia@gmail.com>
2024-04-20 15:44:38 -0300

    feat: Add skeleton component in application

--- src/env.ts
+++ src/env.ts
@@ -2,6 +2,7 @@ import { z as zod } from 'zod';
 
 const envSchema = zod.object({
   VITE_API_URL: zod.string().url(),
+  VITE_ENABLE_API_DELAY: zod.string().transform((value) => value === 'true'),
 });
 
 export const env = envSchema.parse(import.meta.env);
--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,4 +4,16 @@ import { env } from '@/env';
 
 export const api = axios.create({
   baseURL: env.VITE_API_URL,
+  withCredentials: true,
 });
+
+if (env.VITE_ENABLE_API_DELAY) {
+  // Interceptando todas as requisições, ou seja, antes de todas as funções será executado a
+  // função que está dentro de "use".
+  api.interceptors.request.use(async (config) => {
+    // A Promise será resolvida em 2 segundos que foi o tempo que passamos
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    return config;
+  });
+}
